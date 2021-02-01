9b52fe28877e19cb4a98263e53abd602f78819d0
Handa <anandamuhammadmtq@gmail.com>
2021-02-01 12:40:44 +0800

    Add contact list

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {RegisterScreen} from '../screens';
 import {HomeScreen} from '../screens';
+import {ContactList} from '../screens';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,16 @@ const StackScreen = ({navigation}) => {
         name="Home"
         component={HomeScreen}
         options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Contact"
+        component={ContactList}
+        options={{
+          headerShown: true,
+          title: 'Find Receiver',
+          headerStyle: {backgroundColor: '#6379F4', elevation: 0},
+          headerTintColor: '#fff',
+        }}
       />
     </Stack.Navigator>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,5 @@
 import RegisterScreen from './AuthScreen/RegisterScreen';
 import HomeScreen from './HomeScreen';
+import ContactList from './ContactList';
 
-export {RegisterScreen, HomeScreen};
+export {RegisterScreen, HomeScreen, ContactList};
